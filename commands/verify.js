caa9f4be296e6f3546ddb18674f4ab2eeead98ea
Bryan English <bryan@bryanenglish.com>
2016-11-05 13:56:34 -0700

    init verify

---
+++ commands/verify.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const verify = require('../lib/verify');
+
+exports.command = 'verify <attestationfile> <attestedfile>';
+
+exports.describe = 'verify an attestation file';
+
+exports.handler = argv => {
+  let attestationFilename = argv.attestationfile;
+  let attestedFilename = argv.attestedfile;
+
+  if (!path.isAbsolute(attestationFilename)) {
+    attestationFilename = path.resolve(process.cwd(), attestationFilename);
+  }
+  if (!path.isAbsolute(attestedFilename)) {
+    attestedFilename = path.resolve(process.cwd(), attestedFilename);
+  }
+
+  const attestationData = JSON.parse(fs.readFileSync(attestationFilename, 'utf8'));
+  const attestedFileStream = fs.createReadStream(attestedFilename);
+
+  verify(attestationData, attestedFileStream, (err, result) => {
+    if (err) throw err;
+    console.log('File matches shasum', attestationData.data.id);
+    if (result.unknownKey) {
+      console.log(`This attestation is signed with an unknown key: ${attestationData.key}`);
+      console.log('You\'ll need to import it into your keychain.');
+    }
+    console.log('This file is verified, with the following data:')
+    console.log(JSON.stringify(attestationData.data, null, 4));
+  });
+};
